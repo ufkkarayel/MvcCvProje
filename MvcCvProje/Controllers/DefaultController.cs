0bc2da8a5a3fcc0c9727e8f5519509ce178320df
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-10-28 18:28:45 +0300

    Add project files.

---
+++ MvcCvProje/Controllers/DefaultController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvProje.Models.Entity;
+
+namespace MvcCvProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+        public PartialViewResult Egitim()
+        {
+            var egitimler = db.TblEgitimlerim.ToList();
+            return PartialView(egitimler);
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.TblYeteneklerim.ToList();
+            return PartialView(yetenekler);
+        }
+        public PartialViewResult Hobiler()
+        {
+            var hobi = db.TblHobilerim.ToList();
+            return PartialView(hobi);
+        }
+        public PartialViewResult Sertifikalar()
+        {
+            var sertifika = db.TblSertifikalarim.ToList();
+            return PartialView(sertifika);
+        }
+        [HttpGet]
+        public PartialViewResult iletisim()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult iletisim(Tblİletisim t)
+        {
+            t.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.Tblİletisim.Add(t);
+            db.SaveChanges();
+            return PartialView();
+
+        }
+    }
+}
