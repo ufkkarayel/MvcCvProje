05113d97424ba2719a1d0a53814f26714f60a843
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-10-29 19:09:51 +0300

    deneyimler crud işlemleri yapıldı.

---
+++ MvcCvProje/Controllers/YetenekController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvProje.Models.Entity;
+using MvcCvProje.Repositories;
+
+namespace MvcCvProje.Controllers
+{
+    public class YetenekController : Controller
+    {
+        DbCvEntities db =new  DbCvEntities();
+        // GET: Yetenek
+        public ActionResult Index()
+        {
+            var yetenekler = db.TblYeteneklerim.ToList();
+            return View(yetenekler);
+        }
+    }
+}
---
+++ MvcCvProje/Repositories/iletisimRepository.cs
@@ -0,0 +1,12 @@
+﻿using MvcCvProje.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcCvProje.Repositories
+{
+    public class iletisimRepository : GenericRepository<Tblİletisim>
+    {
+    }
+}
