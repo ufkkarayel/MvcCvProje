848bd30664dc925451c2d39843ef1d715e11f0f9
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-10-29 21:30:54 +0300

    yetenek crud işlemleri yapıldı

--- MvcCvProje/Models/Entity/TblYeteneklerim.cs
+++ MvcCvProje/Models/Entity/TblYeteneklerim.cs
@@ -16,5 +16,6 @@ namespace MvcCvProje.Models.Entity
     {
         public int ID { get; set; }
         public string Yetenek { get; set; }
+        public Nullable<byte> Oran { get; set; }
     }
 }
