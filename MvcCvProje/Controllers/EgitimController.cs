e16db9c58d4f42718e0e5afabb5ca3a6861e424c
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-10-30 23:09:10 +0300

    egitim hayatı oluşturuldu.

--- MvcCvProje/Controllers/EgitimController.cs
+++ MvcCvProje/Controllers/EgitimController.cs
@@ -32,5 +32,33 @@ namespace MvcCvProje.Controllers
             repo.TAdd(p);
             return RedirectToAction("Index");
         }
+        public ActionResult EgitimSil(int id)
+        {
+            var egitim = repo.Find(x=>x.ID==id);
+            repo.TDelete(egitim);
+           return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult EgitimDuzenle(int id)
+        {
+            var egitim = repo.Find(x => x.ID == id);
+            return View(egitim);
+        }
+        [HttpPost]
+        public ActionResult EgitimDuzenle(TblEgitimlerim t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("EgitimDuzenle");
+            }
+            var egitim = repo.Find(x => x.ID == t.ID);
+            egitim.Baslik = t.Baslik;
+            egitim.AltBaslik1 = t.AltBaslik1;
+            egitim.AltBaslik2 = t.AltBaslik2;
+            egitim.GNO = t.GNO;
+            egitim.Tarih = t.Tarih;
+            repo.TUpdate(egitim);
+            return RedirectToAction("Index");
+        }
     }
 }
