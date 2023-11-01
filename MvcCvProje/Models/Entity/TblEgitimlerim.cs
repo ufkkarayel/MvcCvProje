1e762e486833e577894d5c2b06d90fd64fab544f
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-11-01 21:43:19 +0300

    Sertifikalar eklendi

--- MvcCvProje/Models/Entity/TblEgitimlerim.cs
+++ MvcCvProje/Models/Entity/TblEgitimlerim.cs
@@ -11,17 +11,13 @@ namespace MvcCvProje.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class TblEgitimlerim
     {
         public int ID { get; set; }
-        [Required (ErrorMessage ="Bu alan boþ býrakýlamaz")]
         public string Baslik { get; set; }
-        
         public string AltBaslik1 { get; set; }
         public string AltBaslik2 { get; set; }
-        [StringLength(10,ErrorMessage ="Lütfen en fazla 10 karakterlik veri giriþi yapýn.")]
         public string GNO { get; set; }
         public string Tarih { get; set; }
     }
