54abff09c2a1bf11034a48fdf84b13e67507014a
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-11-08 21:35:11 +0300

    08.11.2023

---
+++ MvcCvProje/Controllers/iletisimController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvProje.Models.Entity;
+using MvcCvProje.Repositories;
+
+namespace MvcCvProje.Controllers
+{
+    public class iletisimController : Controller
+    {
+        // GET: iletisim
+        GenericRepository<Tblİletisim> repo = new GenericRepository<Tblİletisim>();
+        public ActionResult Index()
+        {
+            var mesajlar = repo.List();
+            return View(mesajlar);
+        }
+    }
+}
